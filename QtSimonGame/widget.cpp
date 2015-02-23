f04749ebb2e656af0fb1e32e01546af347fad63e
PeiBoL79 <pablo.huerta@gmail.com>
2015-02-24 00:22:19 +0100

    Added buttons and colour icons.

--- QtSimonGame/widget.cpp
+++ QtSimonGame/widget.cpp
@@ -1,14 +1,91 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <QPixmap>
+#include <QIcon>
+#include <QFile>
+#include <QDebug>
 
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    InitIcons();
+    InitButtons();
 }
 
 Widget::~Widget()
 {
     delete ui;
+}
+
+void Widget::InitButtons()
+{
+    ui->pushButtonBlue->setIcon(*blueOffIcon);
+    ui->pushButtonGreen->setIcon(*greenOffIcon);
+    ui->pushButtonRed->setIcon(*redOffIcon);
+    ui->pushButtonYellow->setIcon(*yellowOffIcon);
+}
+
+void Widget::InitIcons()
+{
+    QPixmap yellowOn(":Images/Yellow_On.png");
+    yellowOnIcon = new QIcon(yellowOn);
+    QPixmap yellowOff(":Images/Yellow_Off.png");
+    yellowOffIcon = new QIcon(yellowOff);
+
+    QPixmap greenOn(":Images/Green_On.png");
+    greenOnIcon = new QIcon(greenOn);
+    QPixmap greenOff(":Images/Green_Off.png");
+    greenOffIcon = new QIcon(greenOff);
+
+    QPixmap blueOn(":Images/Blue_On.png");
+    blueOnIcon = new QIcon(blueOn);
+    QPixmap blueOff(":Images/Blue_Off.png");
+    blueOffIcon = new QIcon(blueOff);
+
+    QPixmap redOn(":Images/Red_On.png");
+    redOnIcon = new QIcon(redOn);
+    QPixmap redOff(":Images/Red_Off.png");
+    redOffIcon = new QIcon(redOff);
+}
+
+void Widget::on_pushButtonYellow_pressed()
+{
+    ui->pushButtonYellow->setIcon(*yellowOnIcon);
+}
+
+void Widget::on_pushButtonYellow_released()
+{
+    ui->pushButtonYellow->setIcon(*yellowOffIcon);
+}
+
+void Widget::on_pushButtonGreen_pressed()
+{
+    ui->pushButtonGreen->setIcon(*greenOnIcon);
+}
+
+void Widget::on_pushButtonGreen_released()
+{
+    ui->pushButtonGreen->setIcon(*greenOffIcon);
+}
+
+void Widget::on_pushButtonRed_pressed()
+{
+    ui->pushButtonRed->setIcon(*redOnIcon);
+}
+
+void Widget::on_pushButtonRed_released()
+{
+    ui->pushButtonRed->setIcon(*redOffIcon);
+}
+
+void Widget::on_pushButtonBlue_pressed()
+{
+    ui->pushButtonBlue->setIcon(*blueOnIcon);
+}
+
+void Widget::on_pushButtonBlue_released()
+{
+    ui->pushButtonBlue->setIcon(*blueOffIcon);
 }
--- QtSimonGame/widget.h
+++ QtSimonGame/widget.h
@@ -15,8 +15,37 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
+private slots:
+    void on_pushButtonYellow_pressed();
+
+    void on_pushButtonYellow_released();
+
+    void on_pushButtonGreen_pressed();
+
+    void on_pushButtonGreen_released();
+
+    void on_pushButtonRed_pressed();
+
+    void on_pushButtonRed_released();
+
+    void on_pushButtonBlue_pressed();
+
+    void on_pushButtonBlue_released();
+
 private:
+
+    void InitIcons();
+    void InitButtons();
+
     Ui::Widget *ui;
+    QIcon *yellowOnIcon;
+    QIcon *yellowOffIcon;
+    QIcon *greenOnIcon;
+    QIcon *greenOffIcon;
+    QIcon *blueOnIcon;
+    QIcon *blueOffIcon;
+    QIcon *redOnIcon;
+    QIcon *redOffIcon;
 };
 
 #endif // WIDGET_H
