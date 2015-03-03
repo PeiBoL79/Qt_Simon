b42f20e12c5efc28bdf7ac660272648446844763
PeiBoL79 <pablo.huerta@gmail.com>
2015-03-03 22:33:06 +0100

    Added logic and state machine

--- QtSimonGame/widget.cpp
+++ QtSimonGame/widget.cpp
@@ -12,6 +12,8 @@ Widget::Widget(QWidget *parent) :
     ui->setupUi(this);
     InitIcons();
     InitButtons();
+    InitStateMachine();
+    connect(&timer, timer.timeout, this, timeout);
 }
 
 Widget::~Widget()
@@ -58,6 +60,7 @@ void Widget::on_pushButtonYellow_pressed()
 void Widget::on_pushButtonYellow_released()
 {
     ui->pushButtonYellow->setIcon(*yellowOffIcon);
+    emit state_machine_signal(INPUT, YELLOW);
 }
 
 void Widget::on_pushButtonGreen_pressed()
@@ -68,6 +71,7 @@ void Widget::on_pushButtonGreen_pressed()
 void Widget::on_pushButtonGreen_released()
 {
     ui->pushButtonGreen->setIcon(*greenOffIcon);
+    emit state_machine_signal(INPUT, GREEN);
 }
 
 void Widget::on_pushButtonRed_pressed()
@@ -78,6 +82,7 @@ void Widget::on_pushButtonRed_pressed()
 void Widget::on_pushButtonRed_released()
 {
     ui->pushButtonRed->setIcon(*redOffIcon);
+    emit state_machine_signal(INPUT, RED);
 }
 
 void Widget::on_pushButtonBlue_pressed()
@@ -88,4 +93,128 @@ void Widget::on_pushButtonBlue_pressed()
 void Widget::on_pushButtonBlue_released()
 {
     ui->pushButtonBlue->setIcon(*blueOffIcon);
+    emit state_machine_signal(INPUT, BLUE);
+}
+
+void Widget::on_pushButtonStart_clicked()
+{
+    QTime time = QTime::currentTime();
+    qsrand((uint)time.msec());
+
+    for(int i = 0; i < 100; i++)
+    {
+        generatedSequence[i] = (buttonIndex_e)randInt(0,3);
+    }
+
+    emit state_machine_signal(START);
+}
+
+int Widget::randInt(int low, int high)
+{
+// Random number between low and high
+return qrand() % ((high + 1) - low) + low;
+}
+
+void Widget::state_machine_process(enum state_machine_events_e event, enum buttonIndex_e button)
+{
+    switch(stateMachine.state)
+    {
+        case WAIT_START:
+            if(event == START)
+            {
+                stateMachine.state = PLAY_SEQUENCE;
+                timer.setSingleShot(true);
+                timer.start(100);
+            }
+        break;
+        case PLAY_SEQUENCE:
+            if(event == TIMER)
+            {
+                setAllButtonsOff();
+                if(stateMachine.playSequenceIndex != stateMachine.level)
+                {
+                    playSequenceIndex();
+                    stateMachine.playSequenceIndex++;
+                    stateMachine.state = PLAY_SEQUENCE_2;
+                }
+                else
+                {
+                    stateMachine.playSequenceIndex = 0;
+                    stateMachine.state = WAIT_INPUT;
+                }
+                timer.start(500);
+            }
+        break;
+        case PLAY_SEQUENCE_2:
+            setAllButtonsOff();
+            stateMachine.state = PLAY_SEQUENCE;
+            timer.start(100);
+        break;
+        case WAIT_INPUT:
+            if(event == INPUT)
+            {
+                if(button != generatedSequence[stateMachine.waitInputIndex])
+                {
+                    qDebug() << "FIN\n";
+                    stateMachine.state = WAIT_START;
+                    stateMachine.level = 1;
+                    stateMachine.waitInputIndex = 0;
+                }
+                else
+                {
+                    inputSequence[stateMachine.waitInputIndex] = button;
+                    stateMachine.waitInputIndex++;
+                    if(stateMachine.waitInputIndex == stateMachine.level)
+                    {
+                        stateMachine.state = PLAY_SEQUENCE;
+                        stateMachine.waitInputIndex = 0;
+                        stateMachine.level++;
+                        timer.start(500);
+                    }
+                }
+            }
+        break;
+    }
+}
+
+void Widget::setAllButtonsOff()
+{
+    ui->pushButtonYellow->setIcon(*yellowOffIcon);
+    ui->pushButtonBlue->setIcon(*blueOffIcon);
+    ui->pushButtonGreen->setIcon(*greenOffIcon);
+    ui->pushButtonRed->setIcon(*redOffIcon);
+}
+
+void Widget::playSequenceIndex()
+{
+    switch(generatedSequence[stateMachine.playSequenceIndex])
+    {
+        case YELLOW:
+            ui->pushButtonYellow->setIcon(*yellowOnIcon);
+        break;
+        case BLUE:
+            ui->pushButtonBlue->setIcon(*blueOnIcon);
+        break;
+        case GREEN:
+            ui->pushButtonGreen->setIcon(*greenOnIcon);
+        break;
+        case RED:
+            ui->pushButtonRed->setIcon(*redOnIcon);
+        break;
+    }
+}
+
+void Widget::InitStateMachine()
+{
+    stateMachine.level = 1;
+    stateMachine.state = WAIT_START;
+    stateMachine.checkSequenceIndex = 0;
+    stateMachine.playSequenceIndex = 0;
+    stateMachine.waitInputIndex = 0;
+    connect(this, state_machine_signal, this, state_machine_process);
+}
+
+void Widget::timeout()
+{
+    emit state_machine_signal(TIMER);
 }
--- QtSimonGame/widget.h
+++ QtSimonGame/widget.h
@@ -2,10 +2,44 @@
 #define WIDGET_H
 
 #include <QWidget>
+#include <QTime>
+#include <QTimer>
 
 namespace Ui {
 class Widget;
 }
+
+enum state_e
+{
+    WAIT_START = 0,
+    PLAY_SEQUENCE,
+    PLAY_SEQUENCE_2,
+    WAIT_INPUT,
+};
+
+enum state_machine_events_e
+{
+    START = 0,
+    TIMER,
+    INPUT,
+};
+
+enum buttonIndex_e
+{
+    RED = 0,
+    BLUE,
+    GREEN,
+    YELLOW,
+};
+
+typedef struct
+{
+    state_e state;
+    int playSequenceIndex;
+    int waitInputIndex;
+    int checkSequenceIndex;
+    int level;
+} stateMachine_str;
 
 class Widget : public QWidget
 {
@@ -15,27 +49,30 @@ public:
     explicit Widget(QWidget *parent = 0);
     ~Widget();
 
+signals:
+    void state_machine_signal(enum state_machine_events_e event, enum buttonIndex_e button = RED);
+
 private slots:
     void on_pushButtonYellow_pressed();
-
     void on_pushButtonYellow_released();
-
     void on_pushButtonGreen_pressed();
-
     void on_pushButtonGreen_released();
-
     void on_pushButtonRed_pressed();
-
     void on_pushButtonRed_released();
-
     void on_pushButtonBlue_pressed();
-
     void on_pushButtonBlue_released();
+    void on_pushButtonStart_clicked();
+    void state_machine_process(enum state_machine_events_e event, enum buttonIndex_e button = RED);
+    void timeout();
 
 private:
 
     void InitIcons();
     void InitButtons();
+    int randInt(int low, int high);
+    void playSequenceIndex();
+    void setAllButtonsOff();
+    void InitStateMachine();
 
     Ui::Widget *ui;
     QIcon *yellowOnIcon;
@@ -46,6 +83,12 @@ private:
     QIcon *blueOffIcon;
     QIcon *redOnIcon;
     QIcon *redOffIcon;
+    buttonIndex_e generatedSequence[100];
+    buttonIndex_e inputSequence[100];
+    stateMachine_str stateMachine;
+    QTimer timer;
+
+
 };
 
 #endif // WIDGET_H
